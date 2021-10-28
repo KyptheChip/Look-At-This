45a4860002b0068ca6f43f2c27b2a89518d4d9f9
KyptheChip <tottionutciprian@gmail.com>
2021-10-28 14:52:53 +0300

    Add endpoint for images and location listing template

--- src/main/java/com/codecool/lookatthis/controllers/IndexController.java
+++ src/main/java/com/codecool/lookatthis/controllers/IndexController.java
@@ -2,16 +2,21 @@ package com.codecool.lookatthis.controllers;
 
 import com.codecool.lookatthis.models.Location;
 import com.codecool.lookatthis.repository.LocationRepository;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.view.RedirectView;
 
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 @Controller
@@ -31,10 +36,12 @@ public class IndexController {
         return "index";
     }
 
-//    @GetMapping("/")
-//    public List<Location> getAllLocations() {
-//        return locationList;
-//    }
+    @GetMapping("/location-list")
+    public String getAllLocations(Model model) {
+        List<Location> locations = (List<Location>) locationRepository.findAll();
+        model.addAttribute("locations", locations);
+        return "location-list";
+    }
 
     @GetMapping("/add-location")
     public String addLocation() {
@@ -48,7 +55,7 @@ public class IndexController {
             @RequestParam(name = "image") MultipartFile image
     ) throws IOException {
         Location location;
-        if (image != null){
+        if (image != null) {
             location = new Location(title, message, image.getBytes());
         } else {
             location = new Location();
@@ -58,5 +65,16 @@ public class IndexController {
         locationRepository.save(location);
 
         return new RedirectView("/");
+    }
+
+    @GetMapping("/location/image/{id}")
+    public void showProductImage(@PathVariable String id,
+                               HttpServletResponse response) throws IOException {
+        response.setContentType("image/jpeg"); // Or whatever format you wanna use
+
+        Location location = locationRepository.findById(Long.parseLong(id)).get();
+
+        InputStream is = new ByteArrayInputStream(location.getImageData());
+        IOUtils.copy(is, response.getOutputStream());
     }
 }
