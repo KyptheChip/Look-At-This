c82101be56087e4feb554ae1a6b09f651806060a
CozminM <cozmin.mlesnita@gmail.com>
2021-11-11 12:14:39 +0200

    Remove dead code

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -1,6 +1,5 @@
 import React, {useState} from "react";
 import {DisplayMapFC} from "./Map"
-import {Link} from "react-router-dom";
 
 export default function Form() {
   const [location, setLocation] = useState({
