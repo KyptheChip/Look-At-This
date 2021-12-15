e1a62bbaf98ea2f89db86632cf2b492dcf1c4d9e
KypTheChip <tottionutciprian@gmail.com>
2021-12-15 11:58:46 +0200

    Add styling to Location Page

--- frontend/src/components/LocationPage.js
+++ frontend/src/components/LocationPage.js
@@ -37,26 +37,25 @@ export default function LocationPage() {
   }
 
   return (
-    <section className="inner-page">
+    <section className="inner-page flex justify-center">
       <div className="container">
-          <div key={location.id} className="card mb-3" >
-            <div className="row g-0">
-              <div className="col-md-4">
-                <img className="img-fluid rounded-start" src={`data:image/jpeg;base64,${location.imageData}`}
-                     alt=""/>
-                  <EmbedMap location={location}/>
+          <div key={location.id} className="card mb-3 space-y-8 max-w-5xl mx-auto overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800">
+            <img className="img-fluid rounded-start" src={`data:image/jpeg;base64,${location.imageData}`}
+                 alt=""/>
+            <div className="card-body space-y-8">
+              <div>
+                <h1 className="class=block mt-2 text-2xl font-semibold text-gray-800 dark:text-white">{location.title}</h1>
+                <p className="card-text">{location.message}</p>
+                <p>{location.tags.map(tag => <span key={tag.id} id={tag.id}>#{tag.name} </span>)}</p>
               </div>
-              <div className="col-md-8">
-                <div className="card-body">
-                    <h5 className="card-title">{location.title}</h5>
-                  <p className="card-text">{location.message}</p>
-                  <p>{location.tags.map(tag => <span key={tag.id} id={tag.id}>#{tag.name} </span>)}</p>
-                  <p><button className='getstarted' id={location.id} onClick={deleteLocation}>Delete location</button></p>
-                  <p><button className='getstarted' id={location.id}><Link className='getstarted link' to={'/edit-location/' + locationId}>Update location</Link></button></p>
-                </div>
+              <div className="space-y-2">
+                <p><button className='rounded-full bg-green-600 hover:bg-cyan-500 w-1/5' id={location.id} onClick={deleteLocation}>Delete location</button></p>
+                <p><button className='rounded-full bg-green-600 hover:bg-cyan-500 w-1/5' id={location.id}><Link className='getstarted link' to={'/edit-location/' + locationId}>Update location</Link></button></p>
               </div>
             </div>
-
+            <div className="card mb-3 space-y-8 mx-auto overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800 h-full">
+              <EmbedMap location={location}/>
+            </div>
           </div>
       </div>
     </section>
